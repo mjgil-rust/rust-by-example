893a48730736e7c277043e6c5a8f8d7d41d43f98
Malcom Gilbert <malcomgilbert@gmail.com>
2018-01-10 21:17:15 -0500

    first 3

---
+++ 02-comments.rs
@@ -0,0 +1,11 @@
+fn main() {
+  // rust has line comments
+
+  /*
+     it also has block comments
+     like this one here
+  */
+
+  let x = 5 + /* 90 */ 5;
+  println!("Is `x` 10 or 100? x = {}", x)
+}
